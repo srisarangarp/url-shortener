a0187ffeb205e03d3907d1d307a077209df160a6
Srisa Ranga Ramanujam PANCHANGAM <s.panchangam@f5.com>
2021-03-07 16:22:03 +0530

    Divided the main logic into modules

--- main.go
+++ main.go
@@ -12,38 +12,61 @@ import (
 )
 
 func main() {
-	shortenURL("https://www.facebook.com")
+	fmt.Println(shortenURL("https://www.google.com"))
 }
 
-func shortenURL(str string) {
+func shortenURL(str string) string {
+	urlStoreObject, err := getObjectMap("urls-storage.yaml")
+	if err != nil {
+		fmt.Printf("An Error occured in getting the mapObject from yaml file %s", err.Error())
+		return err.Error()
+	}
+
+	if hashString, exists := lookForURL(urlStoreObject, str); exists {
+		return hashString
+	}
+
+	hashString := generateHashString(str, 0)
+	urlStoreObject[str] = hashString
+	data, err := yaml.Marshal(urlStoreObject)
+	if err != nil {
+		fmt.Printf("An Error occured in marshalling the map object %s", err.Error())
+	}
+
+	err = ioutil.WriteFile("urls-storage.yaml", data, 0644)
+	if err != nil {
+		fmt.Printf("An Error occured in writing the map data to yaml file %s", err.Error())
+	}
+	return hashString
+
+}
+
+func generateHashString(str string, startIndex int) string {
 	data := []byte(str)
 	hash := sha256.Sum256(data)
-	yamlFile, err := ioutil.ReadFile("urls-storage.yaml")
-	if err != nil {
-		fmt.Printf("An Error occured in reading the yaml file %s", err.Error())
-	}
+	hashString := hex.EncodeToString(hash[startIndex:7])
+	return hashString
+}
 
+func getObjectMap(path string) (map[string]string, error) {
 	urlStoreObject := make(map[string]string)
+	yamlFile, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
 	fmt.Println(string(yamlFile))
+
 	err = yaml.Unmarshal(yamlFile, &urlStoreObject)
 	if err != nil {
-		fmt.Printf("An Error occured in un marshalling the yaml file %s", err.Error())
+		return nil, err
 	}
+	return urlStoreObject, nil
 
-	if _, exists := urlStoreObject[str]; exists {
-		fmt.Println(urlStoreObject[str])
-	} else {
-		hashString := hex.EncodeToString(hash[0:7])
-		urlStoreObject[str] = hashString
-		data, err = yaml.Marshal(urlStoreObject)
-		if err != nil {
-			fmt.Printf("An Error occured in marshalling the map object %s", err.Error())
-		}
+}
 
-		err = ioutil.WriteFile("urls-storage.yaml", data, 0644)
-		if err != nil {
-			fmt.Printf("An Error occured in writing the map data to yaml file %s", err.Error())
-		}
+func lookForURL(urlStoreObject map[string]string, url string) (string, bool) {
+	if val, exists := urlStoreObject[url]; exists {
+		return val, exists
 	}
-
+	return "", false
 }
